c0195b4ec64a2b69b41ad1a241c32458356fd7a0
Yuki Kirii <shiftky@gmail.com>
2019-01-17 15:56:09 +0900

    Add test case

--- test/mrb_rediscluster.rb
+++ test/mrb_rediscluster.rb
@@ -2,16 +2,29 @@
 ## RedisCluster Test
 ##
 
-assert("RedisCluster#hello") do
-  t = RedisCluster.new "hello"
-  assert_equal("hello", t.hello)
+HOSTS = [
+  { host: '127.0.0.1', port: 7000 },
+  { host: '127.0.0.1', port: 7001 },
+]
+
+assert('Redis#ping') do
+  r = RedisCluster.new(HOSTS)
+
+  assert_equal 'PONG', r.ping
+  r.close
+
+  assert_raise(Redis::ClosedError) { r.ping }
 end
 
-assert("RedisCluster#bye") do
-  t = RedisCluster.new "hello"
-  assert_equal("hello bye", t.bye)
-end
+assert('Redis#set, Redis#get') do
+  r = RedisCluster.new(HOSTS)
 
-assert("RedisCluster.hi") do
-  assert_equal("hi!!", RedisCluster.hi)
+  result = r.set 'hoge', 'fuga'
+  ret = r.get 'hoge'
+  r.close
+
+  assert_equal 'OK', result
+  assert_equal 'fuga', ret
+  assert_raise(Redis::ClosedError) { r.get 'hoge' }
+  assert_raise(Redis::ClosedError) { r.set 'hoge', 'fuga' }
 end
