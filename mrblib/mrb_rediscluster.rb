a2069d18ad1d15c84cac63ae9771fc1bcccdd90b
Yuki Kirii <shiftky@gmail.com>
2019-01-21 19:48:41 +0900

    Support redirection

--- mrblib/mrb_rediscluster.rb
+++ mrblib/mrb_rediscluster.rb
@@ -1,7 +1,8 @@
 class RedisCluster
 
-  DEFAULT_MAX_CACHED_CONNECTIONS = 2
   HASH_SLOTS = 16384
+  MAX_REDIRECTIONS = 16
+  DEFAULT_MAX_CACHED_CONNECTIONS = 2
 
   attr_accessor :slots
   attr_accessor :nodes
@@ -14,6 +15,7 @@ class RedisCluster
     @slots = {}
     @nodes = []
     @connections = {}
+    @refresh_slots_cache = false
 
     initialize_slots_cache
   end
@@ -50,14 +52,50 @@ class RedisCluster
         end
       end
     end
+
+    @refresh_slots_cache = false
   end
 
   def send_cluster_command(argv)
-    key = extract_key(argv)
-    slot = hash_slot(key)
+    asking = false
+    num_redirects = 0
 
-    redis = get_connection_by(slot)
-    return redis.send(argv[0], *argv[1..-1])
+    initialize_slots_cache if @refresh_slots_cache
+
+    begin
+      key = extract_key(argv)
+      slot = hash_slot(key)
+
+      redis = get_connection_by(slot)
+
+      redis.asking if asking
+      asking = false
+
+      return redis.send(argv[0], *argv[1..-1])
+    rescue Redis::ReplyError => e
+      if num_redirects >= MAX_REDIRECTIONS
+        raise "Error: #{argv[0]} #{argv[1..-1].join(' ')} - max redirection limit exceeded (#{MAX_REDIRECTIONS} times)"
+      end
+
+      if err == 'MOVED' || err == 'ASK'
+        err, hash_slot, ip_and_port = e.message.split
+
+        if err == 'ASK'
+          asking = true
+        else
+          @refresh_slots_cache = true
+        end
+
+        newslot = hash_slot.to_i
+        host, port = ip_and_port.split(':')
+
+        unless asking
+          @slots[newslot] = { host: host, port: port, name: ip_and_port }
+        end
+      else
+        raise e
+      end
+    end
   end
 
   def get_connection_by(slot)
