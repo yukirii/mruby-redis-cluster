7891aa3c53536f0cb83e675f56534831e01d0039
Yuki Kirii <shiftky@gmail.com>
2019-02-07 10:38:33 +0900

    Use instance variable to check number of cached connections

--- mrblib/mrb_rediscluster.rb
+++ mrblib/mrb_rediscluster.rb
@@ -152,7 +152,7 @@ class RedisCluster
   end
 
   def close_existing_connection
-    while @connections.length > DEFAULT_MAX_CACHED_CONNECTIONS
+    while @connections.length > @max_cached_connections
       name, conn = @connections.shift
       conn.close
     end
